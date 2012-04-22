b68e46d94eabc70e3f2eea69c6798e5090157ffd
Leonardo <ld.quintero@gmail.com>
2012-04-22 19:20:52 +0200

    Reparar problema del git

--- application/views/comunes/header.php
+++ application/views/comunes/header.php
@@ -6,7 +6,13 @@
  * @author Leoanrdo Quintero
  */
 ?>
-         <div id="header">
-            Cabecera
-            <a href="autenticar">Login</a>
-         </div>
+
+            <div id="header">
+                Cabecera <a href="/">Inicio</a>
+                <br>
+                <a href="<?php echo site_url('cliente') ?>">Cliente</a>
+                <br>
+                <a href="<?php echo site_url('autenticar') ?>">Login</a>
+                <br>
+                <a href="<?php echo site_url('registrar') ?>">Registrar usuario</a>
+            </div>
